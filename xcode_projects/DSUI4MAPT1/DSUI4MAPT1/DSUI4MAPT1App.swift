310ab72979c3116a58aef3afad5738f35e41dc65
Erik Gomez <e@eriknicolasgomez.com>
2021-06-13 20:34:23 -0500

    basic thing

---
+++ xcode_projects/DSUI4MAPT1/DSUI4MAPT1/DSUI4MAPT1App.swift
@@ -0,0 +1,17 @@
+//
+//  DSUI4MAPT1App.swift
+//  DSUI4MAPT1
+//
+//  Created by Erik Gomez on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct DSUI4MAPT1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
