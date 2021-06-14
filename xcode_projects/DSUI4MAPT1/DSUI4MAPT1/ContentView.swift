4642db0872a27aab706303d5a4cb5b751c0490ed
Erik Gomez <e@eriknicolasgomez.com>
2021-06-13 23:33:19 -0500

    finished example

--- xcode_projects/DSUI4MAPT1/DSUI4MAPT1/ContentView.swift
+++ xcode_projects/DSUI4MAPT1/DSUI4MAPT1/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            HeroSubImageView(font: .title2, imageHeight: 35, imageWidth: 35, xOffset: -65, yOffSet: 0, symbolName: "person.crop.circle.badge.checkmark", zIndex: -2)
+            HeroSubImageView(font: .title, imageHeight: 40, imageWidth: 40, xOffset: -35, yOffSet: -20, symbolName: "tray.full.fill", zIndex: -1)
+            HeroSubImageView(font: .largeTitle, imageHeight: 45, imageWidth: 45, xOffset: 0, yOffSet: 0, symbolName: "checkmark.seal.fill", zIndex: 0)
+            HeroSubImageView(font: .largeTitle, imageHeight: 45, imageWidth: 45, xOffset: 35, yOffSet: -20, symbolName: "link.circle.fill", zIndex: -1)
+            HeroSubImageView(font: .title, imageHeight: 40, imageWidth: 40, xOffset: 65, yOffSet: 0, symbolName: "arrow.down.doc.fill", zIndex: -2)
+            HeroSubImageView(font: .title2, imageHeight: 35, imageWidth: 35, xOffset: 95, yOffSet: -20, symbolName: "folder.fill", zIndex: -3)
+        }
+        .frame(width: 300, height: 150)
+    }
+}
+
+struct HeroSubImageView: View {
+    var font: Font
+    var imageHeight, imageWidth, xOffset, yOffSet: CGFloat
+    var symbolName: String
+    var zIndex: Double
+    
+    var body: some View {
+        Image(systemName: symbolName)
+            .font(font)
+            .foregroundColor(.accentColor)
             .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: imageWidth, height: imageHeight)
+                    .foregroundColor(Color(NSColor.textBackgroundColor))
+            )
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.blue, lineWidth: 3)
+                    .opacity(0.4)
+                    .frame(width: imageWidth, height: imageHeight)
+            )
+            .offset(x: xOffset, y: yOffSet)
+            .zIndex(zIndex)
     }
 }
 
